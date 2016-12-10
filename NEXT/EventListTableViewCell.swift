58dd62c8b5c04a78fd1e36a47676a1669e9fcfc0
sheena <sheenamoh@gmail.com>
2016-12-11 00:18:26 +0800

    i am very tired. i wanna sleep now

---
+++ NEXT/EventListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventListTableViewCell.swift
+//  NEXT
+//
+//  Created by Sheena Moh on 10/12/2016.
+//  Copyright © 2016 JJ. All rights reserved.
+//
+
+import UIKit
+
+class EventListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ NEXT/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  NEXT
+//
+//  Created by Sheena Moh on 10/12/2016.
+//  Copyright © 2016 JJ. All rights reserved.
+//
+
+import Foundation
